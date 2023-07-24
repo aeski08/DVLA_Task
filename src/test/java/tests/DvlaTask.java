addec8225ba028737033183bbcdc1dff15cbdaa4
aeski08 <60294078+aeski08@users.noreply.github.com>
2023-07-24 21:56:13 +0100

    first

--- src/test/java/tests/DvlaTask.java
+++ src/test/java/tests/DvlaTask.java
@@ -6,9 +6,15 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DvlaTask {
 
@@ -21,7 +27,7 @@ public class DvlaTask {
 
        //  Filtrelenmiş verileri yazdırma
         for (String[] row : filteredData) {
-            System.out.println(String.join(" ", row));
+          //  System.out.println(String.join(" ", row));
 
         }
         writeValidDataToExcel(filteredData);
@@ -81,24 +87,33 @@ public class DvlaTask {
             String dateOfManufacture = row[3];
 
             // VRN validation
-            if (VRN.matches("[A-Za-z]{2}\\d{2}[A-Za-z]{3} || [A-Za-z]{2}\\d{2}(\\s[A-Za-z]{3})?")) {
+           // if (VRN.matches("[A-Za-z]{2}\\d{2}[A-Za-z]{3}|[A-Za-z]{2}\\d{2}(\\s[A-Za-z]{3})?")) {
+            String regex = "^[A-Z]{2}\\d{2}(\\s?[A-Z]{3})?$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(VRN);
+
+            // VRN validation
+            if (matcher.matches()) {
                 // Make validation
                 if (Make.equalsIgnoreCase("BMW") || Make.equalsIgnoreCase("AUDI") || Make.equalsIgnoreCase("VW") || Make.equalsIgnoreCase("MERCEDES")) {
+
                     // Colour validation
                     if (Colour.equalsIgnoreCase("WHITE") || Colour.equalsIgnoreCase("BLACK") || Colour.equalsIgnoreCase("RED") || Colour.equalsIgnoreCase("BLUE")) {
+
+                        System.out.println("Colour = " + Colour);
                         // Date of Manufacture validation
                         if (isValidDate(dateOfManufacture)) {
-                            filteredList.add(new String[]{VRN, Make, Colour, dateOfManufacture, " --->valid"});
-
+                            System.out.println("dateOfManufacture = " + dateOfManufacture);
+                            filteredList.add(new String[]{VRN, Make, Colour, dateOfManufacture, "valid"});
 
 
                         } else {
-                            filteredList.add(new String[]{VRN, Make, Colour, dateOfManufacture, " --->invalid"});
+                            filteredList.add(new String[]{VRN, Make, Colour, dateOfManufacture, "invalid"});
                         }
                     }
                 }
             } else {
-                filteredList.add(new String[]{VRN, Make, Colour, dateOfManufacture, " ->invalid"});
+                filteredList.add(new String[]{VRN, Make, Colour, dateOfManufacture, "invalid"});
             }
         }
 
@@ -108,16 +123,33 @@ public class DvlaTask {
     // Current date validation
     public static boolean isValidDate(String dateStr) {
         try {
-            String[] dateParts = dateStr.split("-");
-            int day = Integer.parseInt(dateParts[0]);
-            int month = Integer.parseInt(dateParts[1]);
-            int year = Integer.parseInt(dateParts[2]);
+            String[] dateParts;
+            int day = 0;
+            int month = 0;
+            int year = 0;
+            if(dateStr.contains("/")) {
+                dateParts = dateStr.split("/");
+            } else {
+                dateParts = dateStr.split("-");
+             //   System.out.println("dateParts = " + dateStr);
+            }
+            day = Integer.parseInt(dateParts[0]);
+            month = Integer.parseInt(dateParts[1]);
+            year = Integer.parseInt(dateParts[2]);
 
             // Date validation
             if (day > 0 && day <= 31 && month > 0 && month <= 12 && year >= 1900) {
                 // Future date validation
-                String currentDateStr = "26-07-2023"; // Current date
-                String[] currentDateParts = currentDateStr.split("-");
+
+                LocalDate currentDate = LocalDate.now();
+
+                // Format the date as day/month/year
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                String currentDateStr = currentDate.format(formatter);
+
+             //   System.out.println("currentDateStr = " + currentDateStr);
+
+                String[] currentDateParts = currentDateStr.split("/");
                 int currentDay = Integer.parseInt(currentDateParts[0]);
                 int currentMonth = Integer.parseInt(currentDateParts[1]);
                 int currentYear = Integer.parseInt(currentDateParts[2]);
@@ -140,11 +172,12 @@ public class DvlaTask {
         Sheet sheet = workbook.createSheet("ValidData");
 
         // Excel dosyasına başlık (header) ekliyoruz
-        Row headerRow = sheet.createRow(0);
+          Row headerRow = sheet.createRow(0);
         headerRow.createCell(0).setCellValue("VRN");
         headerRow.createCell(1).setCellValue("Make");
         headerRow.createCell(2).setCellValue("Colour");
         headerRow.createCell(3).setCellValue("Date of Manufacture");
+        headerRow.createCell(4).setCellValue("isValid");
 
         int validRowCount = 0;
 
@@ -160,7 +193,7 @@ public class DvlaTask {
                 excelRow.createCell(1).setCellValue(row[1]);
                 excelRow.createCell(2).setCellValue(row[2]);
                 excelRow.createCell(3).setCellValue(row[3]);
-                excelRow.createCell(4).setCellValue("VALID");
+                excelRow.createCell(4).setCellValue(row[4]);
             }
         }
 
